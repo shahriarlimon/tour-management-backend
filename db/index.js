43c6f6f74588ee0f1f1040ce67a4f1d467016dee
shahriar limon <shahriarwebdev@gmail.com>
2022-10-02 11:36:20 +0600

    did some modifications

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-mongoose.connect('mongodb://localhost:27017/tour_management').then(() => { 
+mongoose.connect('mongodb+srv://admin:nnsEamng8RWM3O42@cluster0.eeramyt.mongodb.net/?retryWrites=true&w=majority').then(() => { 
     console.log('db is connected successfully')
 }).catch((error) => {
     console.log('db connection failed',error)
